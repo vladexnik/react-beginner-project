0c843485e02feca14c4534d3633ef17086262b53
Никитюк В.А <NikitukVA@kuzlitmash.org>
2023-07-20 14:30:37 +0300

    convert 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Block } from './Block';
 import './index.scss';
+import axios from 'axios';
 
 function App() {
   const [fromCurrency, setFromCurrency]=useState('rub');
@@ -10,18 +11,29 @@ function App() {
   const [toPrice,setToPrice]=useState(0);
 
 
+  // useEffect(()=>{
+  //   const fetchData=async()=>{
+  //       const response=await fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json')
+  //       const data = await response.json();
+  //       setRates(data[fromCurrency][toCurrency]);
+  //       console.log(data);
+  //   };
+  //   fetchData();
+  // },[fromCurrency])
+
   useEffect(()=>{
     fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${fromCurrency}.json`)
       .then((response)=> response.json())
       .then((json)=>{
         setRates(json[fromCurrency][toCurrency])
+        // onChangeToPrice(1);
         console.log(json[fromCurrency][toCurrency])
       })
       .catch((err)=>{
         console.warn(err);
         alert('mistake to get info');
       });
-  },[])
+  },[fromCurrency])
 
   const onChangeFromPrice=(value)=>{
     // const price=value/rates[fromCurrency];  
@@ -36,6 +48,21 @@ function App() {
     setFromPrice(result)
     setToPrice(value);
   }
+
+  //  const onChangeFromCurrency=(cur)=>{ // когда меняем валюту
+  //   setFromCurrency(cur); // сохр валюту
+  //   onChangeFromPrice(fromPrice); // и паралл сумму от валюты вносим в др часть
+  // }
+
+  React.useEffect(()=>{
+    onChangeFromPrice(fromPrice);
+    
+  },[fromCurrency ,fromPrice])
+
+  React.useEffect(()=>{
+    onChangeToPrice(toPrice);
+    
+  },[toCurrency ,toPrice])
 
   return (
     <div className="App">
